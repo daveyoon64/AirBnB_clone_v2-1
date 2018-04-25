b9da8f512d485efd077f7875f052b11f978eca37
Dave Yoon <daveyoon64@gmail.com>
2018-04-25 22:21:35 +0000

    Update using make_response

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,8 +3,8 @@
     Create app_views and import
 '''
 from flask import Blueprint
-from api.v1.views import *
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-if app_views:
-    from api.v1.views.index import *
+from api.v1.views.index import *
+from api.v1.views import states, amenities, cities,\
+                         places, places_reviews, users
